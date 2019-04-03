04412a3d9db2c1a557fee41fbce05142a77fb7e3
FeliOdras <ariane.feldmann@odras.de>
2019-04-03 11:25:00 +0200

    Added event handler for input field

--- src/MemoList.js
+++ src/MemoList.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './MemoList.css'
 
-export default function MemoList() {
-    return (
-        <div className="memo-list">
-            <h4>My plan for today</h4>
-            <form>
-                <input type="text" id="ListItem"></input>
-                <button>Add</button>
-            </form>
-        </div>
-    )
+export class MemoList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { text: '' }
+    }
+
+    handleChange = (event) => {
+        this.setState({ text: event.target.value })
+    }
+
+    render() {
+        return (
+            <div className="memo-list" >
+                <h4>My plan for today</h4>
+                <form>
+                    <input type="text" id="ListItem" onChange={this.handleChange}></input>
+                    <button>Add</button>
+                </form>
+            </div>
+        )
+    }
 }
+
+export default MemoList
 
