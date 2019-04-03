ec1c638f2a89e499c4e42da3d067c5a2a9241177
FeliOdras <ariane.feldmann@odras.de>
2019-04-03 09:57:52 +0200

    Created component for Logo

---
+++ src/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Logo = () => {
+    return (
+        <div>
+            <img src={logo} className="App-logo" alt="logo" />
+        </div>
+    )
+}
+export default Logo
