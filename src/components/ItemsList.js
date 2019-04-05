5cc4dd73410c4715b9ff059ea9e8fb9f2f0545fa
odras <ariane.feldmann@odras.de>
2019-04-05 16:39:14 +0200

    Added button to delete items

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react'
 import '../styles/ItemsList.css';
 
 export default class ItemsList extends Component {
+
+    handleClick = (event) => {
+        console.log('Clicked')
+    }  
+    
+
+
     render() {
         return (
             <div>
                 <ul>
                     {this.props.items.map(item => {
                         return (
-                            <li key={item.id}>{item.text}</li>
+                            <li key={item.id}>{item.text} <button className="delete" onClick={this.handleClick}>delete</button></li>
                         )
                     })}
                 </ul>
