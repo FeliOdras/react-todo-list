95f9c5aa5f5c081b7ecbb9759daf1b486b5c1efd
FeliOdras <ariane.feldmann@odras.de>
2019-04-11 11:57:52 +0200

    Created method to remove certain value from array

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react'
 import '../styles/ItemsList.css';
 
 export default class ItemsList extends Component {
+    constructor(props) {
+        super(props)
+        this.localStorage = JSON.parse(localStorage.getItem('items'))
+    }
+
+    handleDelete = (event) => {
+        const itemsList = this.localStorage;
+        const itemID = parseInt(event.target.id);
+        for (let i = 0; i < itemsList.length; i++) {
+            let index = itemsList.indexOf(itemsList[i]);
+            if (itemID === itemsList[i].id) {
+                itemsList.splice(index, 1);
+                localStorage.items = ('items', JSON.stringify(itemsList));
+            } else {
+                console.log('Keep ' + itemsList.indexOf(itemsList[i]))
+            }
+        }
+    }
+
     render() {
         return (
             <div>
                 <ul>
                     {this.props.items.map(item => {
                         return (
-                            <li key={item.id}>{item.text}</li>
+                            <li key={item.id}>{item.text}<button id={item.id} onClick={this.handleDelete}>Remove</button></li>
                         )
                     })}
                 </ul>
